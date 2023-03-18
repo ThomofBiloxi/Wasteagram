4e092c552f04efffd3fef2aa774851eca33783c5
Thomas Gantt <tomdgantt@outlook.com>
2023-03-18 14:34:46 -0700

    created scaffold for project changed theme mode to dark mode created model class for new entry

---
+++ lib/model/new_post.dart
@@ -0,0 +1,16 @@
+class Post {
+  final DateTime date;
+  final String imageURL;
+  final int quantity;
+  final double latitude;
+  final double longitude;
+
+  Post({
+    required this.date,
+    required this.imageURL, 
+    required this.quantity,
+    required this.latitude,
+    required this.longitude
+    });
+
+}
